519f29a70fdcf2055d736509ce3287cd841298b3
Shajid <shajid1biz@gmail.com>
2023-08-10 16:38:00 +0530

    Added buttons via Javascript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image')
+const bottom = document.querySelector('.bottom');
 
 
 let sliderNumber = 1;
@@ -14,14 +15,30 @@ const nextSlide = () => {
 }
 
 const prevSlide = () => {
+    slider.style.transform = `translateX(-${(sliderNumber-2) * 700}px)`
+    sliderNumber --;
+}
+
+const getFirstSlide = () => {
     slider.style.transform = `translateX(0px)`
     sliderNumber = 1;
 }
 
+const getLastSlide = () => {
+    slider.style.transform = `translateX(-${(length - 1) * 700}px)`;
+    sliderNumber = length;
+}
+
 right.addEventListener('click', () => {
-    sliderNumber < length ? nextSlide() : prevSlide();
+    sliderNumber < length ? nextSlide() : getFirstSlide();
 }); 
 
-// 700
-// 1400
-// 2     - for the last image
+left.addEventListener('click', () => {
+    sliderNumber > 1 ? prevSlide() : getLastSlide();
+})
+
+for (let i = 0; i < length; i++) {
+    const div = document.createElement("div")
+    div.className = 'button';
+    bottom.appendChild(div)
+}
