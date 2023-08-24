489efdeae67778230a6885a1ec5278e322b1eae8
Sajid <sajid@duck.com>
2023-08-24 19:22:07 +0530

    Organized the code and added comments

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// Constants
+const slideWidth = 700;
+const images = document.querySelectorAll('.image');
+const length = images.length;
+
+// Elements
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.image')
 const bottom = document.querySelector('.bottom');
-const bodyBg = document.querySelector('body');
 
+// State
 let sliderNumber = 1;
 
-let length = images.length;
-
+// Create buttons
 for (let i = 0; i < length; i++) {
     const div = document.createElement("div")
     div.className = 'button';
-    bottom.appendChild(div)
+    bottom.appendChild(div);
 }
 
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".button");
 buttons[0].style.backgroundColor = 'white';
 
+// Functions
 const resetBg = () => {
     buttons.forEach((button) => {
         button.style.backgroundColor = 'transparent'
@@ -29,19 +34,19 @@ buttons.forEach((button, i) => {
     button.addEventListener('click', ()=> {
         resetBg();
         dynamicBg();
-        slider.style.transform = `translateX(-${i * 700}px)`;
+        slider.style.transform = `translateX(-${i * slideWidth}px)`;
         // slideNumber = i + 1;
         button.style.backgroundColor = 'white';
     })
 })
 
 const nextSlide = () => {
-    slider.style.transform = `translateX(-${sliderNumber * 700}px)`
+    slider.style.transform = `translateX(-${sliderNumber * slideWidth}px)`
     sliderNumber ++;
 }
 
 const prevSlide = () => {
-    slider.style.transform = `translateX(-${(sliderNumber-2) * 700}px)`
+    slider.style.transform = `translateX(-${(sliderNumber-2) * slideWidth}px)`
     sliderNumber --;
 }
 
@@ -51,10 +56,11 @@ const getFirstSlide = () => {
 }
 
 const getLastSlide = () => {
-    slider.style.transform = `translateX(-${(length - 1) * 700}px)`;
+    slider.style.transform = `translateX(-${(length - 1) * slideWidth}px)`;
     sliderNumber = length;
 }
 
+// Event listeners
 right.addEventListener('click', () => {
     sliderNumber < length ? nextSlide() : getFirstSlide();
     resetBg();
